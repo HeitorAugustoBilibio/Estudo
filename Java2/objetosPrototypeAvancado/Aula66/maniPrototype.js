487aa2b7e54fe857ded7ece605ad642af6e2d9a0
Heitor <heitoraugustobilibio4@gmail.com>
2023-10-23 17:50:53 -0300

    Aula 68 67 Objeto

--- Java2/objetosPrototypeAvancado/Aula66/maniPrototype.js
+++ Java2/objetosPrototypeAvancado/Aula66/maniPrototype.js
@@ -1,3 +1,4 @@
+/*
 //new Object -> Object.prototype
 const objA = {
     chaveA: 'A',
@@ -15,6 +16,46 @@ objC.chaveC = 'C';
 Object.setPrototypeOf(objB, objA)
 Object.setPrototypeOf(objC, objB)
 console.log(objC.chaveB)
+*/
+
+function Produto(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+};
+Object.setPrototypeOf(p2, Produto.prototype);   
+
+p2.aumento(10)
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 113
+    },
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 142  
+    }
+});
+p3.aumento(10)
+console.log(p3)
 
 // NUNCA USAR PROTOTYPE(SIM VOCÊ APRENDEU ISSO DE MEME)
-//TEM MAIS EXEMPLO MAS FDS
+
---
+++ Java2/objetosPrototypeAvancado/Aula68/validacaoCPF.js
@@ -0,0 +1,21 @@
+/*--------------------------Logica-------------------------
+CPF 106 609 919 - 77
+
+1X 0X 6X 6X 0x 9x 9x 1x 9x      
+10 9  8  7  6  5  4  3  2
+10 0 48 42  0 45 36  3 18 = 202 
+
+11 - (202 % 11) = 7 (Primeiro digito)
+//Se o numero for maior que 9, consideramos 0 
+
+1X 0X 6X 6X 0x 9x 9x 1x 9x 7x
+11 10 9  8  7  6  5  4  3  2
+11 0 54 48  0 54 45  4 27 14 = 257
+
+11 - (257 % 11) = 7 (Segundo digito)
+//Se o numero for maior que 9, consideramos 0 
+
+106.609.919.77 === 106.609.919.77
+
+*/
+
