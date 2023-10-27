3a608640f8ff8b35bcb8c64df841d1a3369c9ae4
Heitor <heitoraugustobilibio4@gmail.com>
2023-10-27 17:53:36 -0300

    Inicio Tooling e ES6 Modeles

--- Java2/JavaScriptAssincrono/Aula81_Ajax(http)/assets/js/main.js
+++ Java2/JavaScriptAssincrono/Aula81_Ajax(http)/assets/js/main.js
@@ -1,19 +1,19 @@
-const request = obj =>  {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest ();
-        xhr.open(obj.method, obj.url, true) ;
-        xhr.send();
+// const request = obj =>  {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest ();
+//         xhr.open(obj.method, obj.url, true) ;
+//         xhr.send();
     
     
-        xhr.addEventListener('load', () => {
-            if(xhr.status >= 200 && xhr.status < 300){
-                resolve(xhr.responseText);
-            } else {
-                reject(xhr.statusText)
-            }
-        });
-    });
-};
+//         xhr.addEventListener('load', () => {
+//             if(xhr.status >= 200 && xhr.status < 300){
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText)
+//             }
+//         });
+//     });
+// };
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -25,23 +25,53 @@ document.addEventListener('click', e => {
     }
 });
 
- async function carregaPagina(el){
-    const href = el.getAttribute('href');
+ 
     
 
-    const objConfig = {
-        method: 'GET',
-        url: href,
-    };
-    try{
-        const  response = await request(objConfig);
-        CarregaResultado(response);
-    } catch(e){
-        console.log(e)
-    }
+    // const objConfig = {
+    //     method: 'GET',
+    //     url: href,
+    // };
+
+
+    async function carregaPagina(el){
+        try{
+            const href = el.getAttribute('href');
+            const response = await fetch(href)
+        
+            if(response.status !== 200) throw new Error('ERRO 404!!!');
+        
+        
+            const html = await response.text()
+            CarregaResultado(html)
+        } catch(e) {
+            console.log(e)
+        }
 }
+    // fetch(href)
+    //     .then(response => {
+    //         if(response.status !== 200) throw new Error('ERRO 404!!!');
+    //         return response.text()
+    //     })
+    //     .then(html => CarregaResultado(html))
+    //     .catch (e => console.log(e))
+
+    
+
   
 function CarregaResultado(response){
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 }
+
+
+
+
+fetch('pagina.html')
+    .then(resposta => {
+        if(resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+        return resposta.text();
+    })
+    .then(html => console.log(html))
+    .catch(e => console.error(e))
+    //.catch(e => console.warn(e))
---
+++ Java2/Javascript Tooling e ES6 Modeles/Aula84/webpack.config.js
@@ -0,0 +1,5 @@
+const path = require('path'); // CommomJS
+
+module.exports = {
+    mode: 'development'
+};
