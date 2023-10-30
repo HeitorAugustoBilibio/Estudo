83962339b1fd95f396f745c424a000f757ecb3b2
unknown <heitoraugustobilibio4@gmail.com>
2023-10-29 23:41:54 -0300

    Sofrencia pra configura webpack

--- Java2/Javascript Tooling e ES6 Modeles/Aula84/src/index.js
+++ Java2/Javascript Tooling e ES6 Modeles/Aula84/src/index.js
@@ -1,4 +1,5 @@
-// 1
-// 2
-// 3
-console.log('EIIIIIIIIIIIIIIIIIIIIIII');
+import {nome2, sobrenome, idade, soma} from './module1';
+
+
+console.log(nome2, sobrenome, idade);
+console.log(soma(5, 6))
