575c669209f70f13d8cbd4c83ab11335394380a1
Heitor <heitoraugustobilibio4@gmail.com>
2023-11-07 17:53:29 -0300

    Lançamento de usuario na agenda

--- Java2/ProjetoAgenda/Agenda/src/controllers/loginController.js
+++ Java2/ProjetoAgenda/Agenda/src/controllers/loginController.js
@@ -6,16 +6,27 @@ exports.index = (req, res) => {
 };
 
 exports.register = async (req, res) => {
-    const login = new Login(req.body);
-    await login.register();
-
-    if(login.errors.length > 0) {
-        req.flash('errors', login.errors);
+    try {
+        const login = new Login(req.body);
+        await login.register();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+                return res.redirect('back');
+            });
+            return;
+        }
+    
+        req.flash('success', 'Seu usuario foi criado com sucesso');
         req.session.save(function() {
             return res.redirect('back');
         });
-        return;
-    }
 
-    res.send(login.errors);
+
+    } catch(e){
+        console.log(e)
+        return res.render('404')
+    }
+    
 };
