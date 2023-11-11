8d176e8e780a2a903e7a612b5e393f3ba648fd93
Heitor Augusto Bilibio <heitoraugustobilibio4@gmail.com>
2023-11-11 02:31:38 -0300

    Terminando de vez Agenda

--- Java2/ProjetoAgenda/Agenda/frontend/main.js
+++ Java2/ProjetoAgenda/Agenda/frontend/main.js
@@ -1,5 +1,18 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import './assets/css/style.css';
+import Login from './modules/Login';
+import Contatos from './modules/Contatos';
 
+//Login
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
+
+//Contato
+
+const contato = new Contatos('.form-contato');
+
+contato.init()
---
+++ Java2/ProjetoAgenda/Agenda/frontend/modules/Contatos.js
@@ -0,0 +1,31 @@
+import validator from "validator";
+
+export default class Contatos {
+    constructor(formClass){
+        this.form = document.querySelector(formClass);
+    }
+
+    init() {
+        this.events();
+    }
+
+    events(){
+        if (!this.form) return;
+        this.form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          this.valida(e)
+        });
+    }
+
+    valida(e){
+        const el = e.target;
+        const emailInput = el.querySelector('input[name="email"]')
+        let error = false;
+
+        if(!validator.isEmail(emailInput.value)){
+            alert('TA ERRADO ESSE EMAIL EM')
+            error = true;
+        }
+        if(!error) el.submit()
+    }
+}
