a54077b738a1b83c94fdb4510335e33ae4f4abe7
Heitor Augusto Bilibio <heitoraugustobilibio4@gmail.com>
2023-11-20 00:59:15 -0300

    Estrutura do Projeto

--- Java2/ApiRest/app.js
+++ Java2/ApiRest/app.js
@@ -1,3 +1,21 @@
-const nome = 'Heitor';
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
 
-console.log(nome);
+class App {
+  constructor() {
+    this.app = express();
+    this.middleware();
+    this.routes();
+  }
+
+  middleware() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
