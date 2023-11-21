114d8416349c0b65396d0e33c9c94175071eaf79
Heitor Augusto Bilibio <heitoraugustobilibio4@gmail.com>
2023-11-21 17:50:22 -0300

    Criando Usuario1

--- Java2/ApiRest/src/controllers/HomeController.js
+++ Java2/ApiRest/src/controllers/HomeController.js
@@ -3,12 +3,12 @@ import Aluno from '../models/Aluno';
 class HomeController {
   async index(req, res) {
     const novoAluno = await Aluno.create({
-      nome: 'Maysa',
-      sobrenome: 'Vidi',
-      email: 'maysavidi@gmail.com',
-      idade: 17,
-      peso: 54,
-      altura: 1.65,
+      nome: 'Maria',
+      sobrenome: 'Moro',
+      email: 'mariamoro@gmail.com',
+      idade: 18,
+      peso: 57,
+      altura: 1.70,
     });
     res.json(novoAluno);
   }
