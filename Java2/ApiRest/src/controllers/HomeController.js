4751330957ac7ea3e0e577cf088fc7ea0ace9763
Heitor Augusto Bilibio <heitoraugustobilibio4@gmail.com>
2023-11-21 00:50:54 -0300

    Criando Primeiros Alunos

--- Java2/ApiRest/src/controllers/HomeController.js
+++ Java2/ApiRest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Maysa',
+      sobrenome: 'Vidi',
+      email: 'maysavidi@gmail.com',
+      idade: 17,
+      peso: 54,
+      altura: 1.65,
     });
+    res.json(novoAluno);
   }
 }
 export default new HomeController();
